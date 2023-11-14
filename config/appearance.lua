b8e321b6221c28f6b976f4b765f32d26c6f2cf9b
钱松 <2744588786@qq.com>
2023-11-14 19:16:10 +0800

    修改了背景图片与透明度

--- config/appearance.lua
+++ config/appearance.lua
@@ -22,7 +22,7 @@ return {
          source = { Color = colors.background },
          height = '100%',
          width = '100%',
-         opacity = 0.00,
+         opacity = 0.85,
       },
    },
 
