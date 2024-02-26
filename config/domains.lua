ff468401cc7c462942b88aad472eb25b1f0d0443
钱松 <2744588786@qq.com>
2024-02-26 15:16:32 +0800

    添加了Almalinux域连接配置

--- config/domains.lua
+++ config/domains.lua
@@ -26,6 +26,31 @@ return {
             identityfile = 'C:\\Users\\Fizz\\.ssh\\id_rsa',
          },
       },
+      {
+         -- The connection is an ssh connection, don't use any multiplexing.
+         multiplexing = 'None',
+
+         -- The name of this specific domain. Must be unique amongst
+         name = 'Alma-linux',
+
+         -- identifies the host:port pair of the remote server
+         -- Can be a DNS name or an IP address with an optional
+         -- ":port" on the end.
+         remote_address = 'host.myalmalinux.com:22',
+
+         -- Whether agent auth should be disabled.
+         -- Set to true to disable it.
+         -- no_agent_auth = false,
+
+         -- The username to use for authenticating with the remote host
+         username = 'root',
+
+         -- Specify a SSH connection authentication file
+         -- Default authentication file is "~/.ssh/id_rsa"
+         ssh_option = {
+            identityfile = 'C:\\Users\\Fizz\\.ssh\\id_rsa',
+         },
+      },
    },
 
    -- ref: https://wezfurlong.org/wezterm/multiplexing.html#unix-domains
